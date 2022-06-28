10ae3882765105056f28efe087d0616c3f31aab0
Matthew Abraham <wasteofintel@gmail.com>
2022-06-28 09:50:04 -0400

    style: integrate new fonts

--- src/styles/fonts.tsx
+++ src/styles/fonts.tsx
@@ -1,36 +1,35 @@
 import { StyleSheet } from 'react-native'
 import colors from 'src/styles/colors'
 
-const Inter = {
-  Regular: 'Inter-Regular',
-  Medium: 'Inter-Medium',
-  SemiBold: 'Inter-SemiBold',
+const Poppins = {
+  SemiBold: 'Poppins-SemiBold',
 }
 
-const Jost = {
-  Book: 'Jost-Book',
-  Medium: 'Jost-Medium',
+const Lato = {
+  Regular: 'Lato-Regular',
+  Medium: 'Lato-Medium',
+  Bold: 'Lato-Bold',
 }
 
-export const fontFamily = Inter.Regular
+export const fontFamily = Poppins.SemiBold
 
 const standards = {
   large: {
-    fontSize: 18,
-    lineHeight: 24,
-    fontFamily: Inter.Regular,
+    fontSize: 28,
+    lineHeight: 32,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
   regular: {
-    fontSize: 16,
-    lineHeight: 22,
-    fontFamily: Inter.Regular,
+    fontSize: 20,
+    lineHeight: 24,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
   small: {
     fontSize: 14,
-    lineHeight: 18,
-    fontFamily: Inter.Regular,
+    lineHeight: 20,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
 }
@@ -39,73 +38,73 @@ const fontStyles = StyleSheet.create({
   h1: {
     fontSize: 26,
     lineHeight: 32,
-    fontFamily: Jost.Book,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
   h2: {
     fontSize: 22,
     lineHeight: 28,
-    fontFamily: Jost.Medium,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
   sectionHeader: {
     fontSize: 14,
     lineHeight: 16,
-    fontFamily: Inter.Medium,
+    fontFamily: Lato.Medium,
     color: colors.dark,
   },
   navigationHeader: {
     fontSize: 16,
     lineHeight: 20,
-    fontFamily: Jost.Medium,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
   notificationHeadline: {
     fontSize: 18,
     lineHeight: 22,
-    fontFamily: Jost.Medium,
+    fontFamily: Lato.Medium,
     color: colors.dark,
   },
   displayName: {
     fontSize: 18,
     lineHeight: 24,
-    fontFamily: Jost.Medium,
+    fontFamily: Lato.Medium,
     color: colors.dark,
   },
   label: {
     fontSize: 13,
     lineHeight: 16,
-    fontFamily: Inter.SemiBold,
+    fontFamily: Lato.Medium,
     color: colors.dark,
   },
   large: standards.large,
   regular: standards.regular,
   small: standards.small,
-  large600: { ...standards.large, fontFamily: Inter.SemiBold },
-  regular600: { ...standards.regular, fontFamily: Inter.SemiBold },
-  small600: { ...standards.small, fontFamily: Inter.SemiBold },
-  large500: { ...standards.large, fontFamily: Inter.Medium },
-  regular500: { ...standards.regular, fontFamily: Inter.Medium },
-  small500: { ...standards.small, fontFamily: Inter.Medium },
-  small400: { ...standards.small, fontFamily: Inter.Regular },
+  large600: { ...standards.large, fontFamily: Poppins.SemiBold },
+  regular600: { ...standards.regular, fontFamily: Poppins.SemiBold },
+  small600: { ...standards.small, fontFamily: Poppins.SemiBold },
+  large500: { ...standards.large, fontFamily: Poppins.SemiBold },
+  regular500: { ...standards.regular, fontFamily: Poppins.SemiBold },
+  small500: { ...standards.small, fontFamily: Poppins.SemiBold },
+  small400: { ...standards.small, fontFamily: Poppins.SemiBold },
   center: {
     textAlign: 'center',
   },
   mediumNumber: {
     lineHeight: 27,
     fontSize: 24,
-    fontFamily: Inter.Regular,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
   largeNumber: {
     lineHeight: 40,
     fontSize: 32,
-    fontFamily: Inter.SemiBold,
+    fontFamily: Poppins.SemiBold,
     color: colors.dark,
   },
   iconText: {
     fontSize: 16,
-    fontFamily: Inter.Medium,
+    fontFamily: Poppins.SemiBold,
     color: colors.light,
   },
   emptyState: {
