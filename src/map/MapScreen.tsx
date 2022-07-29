166473c2dfdbbcff6be9cc1296a5d75102736bfb
Matthew Abraham <wasteofintel@gmail.com>
2022-07-29 13:45:11 -0400

    feat: add map component

--- src/map/MapScreen.tsx
+++ src/map/MapScreen.tsx
@@ -1,42 +1,29 @@
-import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useDispatch } from 'react-redux'
-import { BASE_TAG } from 'src/map/constants'
-import { Screens } from 'src/navigator/Screens'
-import { StackParamList } from 'src/navigator/types'
-import MapboxGL from '@rnmapbox/maps'
-
-MapboxGL.setAccessToken(
-  'sk.eyJ1IjoibWFya3BlcmVpciIsImEiOiJjbDRlaThramowM3NmM2lvMzkyamNyeWgwIn0.PqiVHH7G4ZLRGWCklu_nJA'
-)
-
-type Props = StackScreenProps<StackParamList, Screens.Map>
-
-function MapScreen({ route }: Props) {
-  const LOG_TAG = BASE_TAG + 'Screen'
-
-  const { params: routeParams } = route || {}
-  const { t } = useTranslation()
-  const dispatch = useDispatch()
-
-  return (
-    <SafeAreaView style={styles.container} edges={['bottom']}>
-      <MapboxGL.MapView style={styles.map} />
-    </SafeAreaView>
-  )
-}
+import { StyleSheet, View } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps' // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingHorizontal: 8,
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'red',
   },
   map: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'green',
   },
 })
 
-export default MapScreen
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    ></MapView>
+  </View>
+)
