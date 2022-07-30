e125d3687a59b249aef9483c5eeb8cbe3748fca6
Matthew Abraham <wasteofintel@gmail.com>
2022-07-30 13:23:21 -0400

    feat: add default map integration

--- src/map/MapScreen.tsx
+++ src/map/MapScreen.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps' // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, { Marker } from 'react-native-maps'
+
+const MapScreen = () => (
+  <View style={styles.container}>
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    ></MapView>
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -13,17 +27,4 @@ const styles = StyleSheet.create({
   },
 })
 
-export default () => (
-  <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}
-    ></MapView>
-  </View>
-)
+export default MapScreen
