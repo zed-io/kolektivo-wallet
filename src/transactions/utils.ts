30cbb2a8c3d95125ff556324e3e5f04e93d3b8b5
Matthew Abraham <wasteofintel@gmail.com>
2022-08-26 12:39:46 -0400

    feat: implement design for export

--- src/transactions/utils.ts
+++ src/transactions/utils.ts
@@ -1,8 +1,16 @@
+import BigNumber from 'bignumber.js'
+import { filter, forEach } from 'lodash'
+import { useSelector } from 'react-redux'
 import { TokenTransactionType } from 'src/apollo/types'
+import { formatValueToDisplay } from 'src/components/TokenDisplay'
 import { ExchangeConfirmationCardProps } from 'src/exchange/ExchangeConfirmationCard'
 import i18n from 'src/i18n'
 import { AddressToDisplayNameType } from 'src/identity/reducer'
+import { localCurrencyToUsdSelector } from 'src/localCurrency/selectors'
+import { useTokenInfo } from 'src/tokens/hooks'
+import { FeedTokenTransaction } from 'src/transactions/feed/TransactionFeed'
 import { TransferConfirmationCardProps } from 'src/transactions/TransferConfirmationCard'
+import { TokenTransfer } from 'src/transactions/types'
 import { Currency } from 'src/utils/currencies'
 import { formatFeedSectionTitle, timeDeltaInDays } from 'src/utils/time'
 
@@ -94,4 +102,56 @@ export const transferReviewHeader = (
   }
 
   return headerText
+}
+
+export const filterTxByMonth = (
+  transactions: FeedTokenTransaction[],
+  month: string | undefined
+) => {
+  if (!month) return transactions
+  return filter(transactions, (tx) => {
+    const target = formatFeedSectionTitle(tx.timestamp, i18n)
+    return target === month
+  })
+}
+
+export const filterTxByAmount = (
+  transactions: FeedTokenTransaction[],
+  minAmount = Number.MIN_SAFE_INTEGER,
+  maxAmount = Number.MAX_SAFE_INTEGER
+) => {
+  if (minAmount === Number.MIN_SAFE_INTEGER && maxAmount === Number.MAX_SAFE_INTEGER)
+    return transactions
+  return filter(transactions, (tx: TokenTransfer) => {
+    const amount = Math.abs(tx.amount.localAmount?.value as number)
+    return amount >= minAmount && amount <= maxAmount
+  }) as FeedTokenTransaction[]
+}
+
+export const filterTxByRecipient = (
+  transactions: FeedTokenTransaction[],
+  recipient: string | undefined
+) => {
+  return recipient
+    ? (filter(transactions, (tx: TokenTransfer) => {
+        return tx.address === recipient
+      }) as FeedTokenTransaction[])
+    : transactions
+}
+
+export function calculateTransactionSubtotal(transactions: FeedTokenTransaction[]) {
+  let total: BigNumber = new BigNumber(1)
+  forEach(transactions, (transaction: FeedTokenTransaction) => {
+    const transfer = transaction as TokenTransfer
+    const tokenAmount = transfer.amount.value
+    const tokenAddress = transfer.amount.tokenAddress
+    const tokenInfo = useTokenInfo(tokenAddress)
+    const localCurrencyExchangeRate = useSelector(localCurrencyToUsdSelector)
+    const amountInUsd = tokenInfo?.usdPrice?.multipliedBy(tokenAmount)
+    const amountInLocalCurrency = new BigNumber(localCurrencyExchangeRate ?? 0).multipliedBy(
+      amountInUsd ?? 0
+    )
+    total = total.plus(amountInLocalCurrency)
+  })
+  return formatValueToDisplay(total)
 }
