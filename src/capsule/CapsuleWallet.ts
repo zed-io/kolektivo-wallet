4de1fa94d89d9df40d50678dbf1d04c22829d12f
Michał Osadnik <micosa97@gmail.com>
2022-12-30 20:34:17 +0200

    Set up cookie refreshing skeleton (#7)

--- src/capsule/CapsuleWallet.ts
+++ src/capsule/CapsuleWallet.ts
@@ -6,10 +6,43 @@ import * as ethUtil from 'ethereumjs-util'
 import { ErrorMessages } from 'src/app/ErrorMessages'
 import { CapsuleSigner } from 'src/capsule/CapsuleSigner'
 import Logger from 'src/utils/Logger'
+import { ChallengeStorageDefault } from './ChallengeStorage'
+import userManagementClient from './UserManagementClient'
 
 const TAG = 'geth/CapsuleWallet'
 
 export class CapsuleWallet extends RemoteWallet<CapsuleSigner> implements UnlockableWallet {
+  // TODO remove me
+  private userId = 'c67b0766-f339-4d86-9c82-fe2410b28460'
+  private biometricStorage = new ChallengeStorageDefault(this.userId)
+
+  public async setBiometrics() {
+    return await userManagementClient.addBiometrics(this.userId, {
+      publicKey: await this.biometricStorage.getPublicKey(),
+    })
+  }
+
+  private cookie: string | undefined
+
+  public async refreshBiometricsIfNeeded() {
+    if (typeof this.cookie === 'string') {
+      // this is how cookie is represented. We do parsing "manually" to avoid employing additional libs
+      // Example cookie: capsule.sid=s%3Ad324cb79-96c8-4995-868b-4774ae2004ce.RZ2H%2BbendbOVXEBJ2tKVLatSh24SOxxQ%2F7A51lfdSoM; Path=/; Expires=Fri, 30 Dec 2022 18:31:47 GMT; HttpOnly; SameSite=Strict
+      const expDate = this.cookie.split?.(';')?.[2]?.split?.('=')?.[1]
+      const isValid = expDate && new Date(expDate).valueOf() - Date.now() > 30000 // 30 seconds threshold
+      if (isValid) {
+        return
+      }
+    }
+    const challenge = await userManagementClient.getBiometricsChallenge(this.userId)
+    const message = challenge.data.challenge
+    const signature = await this.biometricStorage.signChallenge(message)
+    const response = await userManagementClient.verifyBiometricsChallenge(this.userId, {
+      signature,
+    })
+    this.cookie = response.headers['set-cookie'][0]
+  }
+
   // Called on init to load existing wallets
   // Not applicable for CapsuleWallet
   async loadAccountSigners(): Promise<Map<string, CapsuleSigner>> {
--- src/capsule/ChallengeStorage.ts
+++ src/capsule/ChallengeStorage.ts
@@ -1,31 +1,43 @@
+import elliptic from 'elliptic'
+import crypto from 'crypto'
+
 export abstract class ChallengeStorage {
-  public publicKey: string
   protected userId: string
 
-  protected abstract generatePair(): string
+  // returns public key and generates pair (if needed)
+  public abstract getPublicKey(): Promise<string>
 
-  protected abstract getPublicKey(): string | undefined
-
-  public abstract signChallenge(message: string): string
+  public abstract signChallenge(message: string): Promise<Signature>
 
   public constructor(userId: string) {
     this.userId = userId
-    const publicKey = this.getPublicKey()
-    this.publicKey = publicKey ?? this.generatePair()
   }
 }
 
-// TODO
+export interface Signature {
+  r: string
+  s: string
+  recoveryParam: number
+}
+
+const ec = new elliptic.ec('p256')
+
+const privateKey = '202d73cbde65f547c75613ace311393ac97f2556cbe3aca32bf48eb84ec2198c'
 export class ChallengeStorageDefault extends ChallengeStorage {
-  protected generatePair(): string {
-    return ''
+  async getPublicKey(): Promise<string> {
+    return '0483326f8677519eace4e8db81722399ac4b581a91236656359ebf3621ad3186fdf2e1fa04c9929d577c36ffb9e2ef6cfe325d1da7ffa4d0a596bf88d7e335baf2'
   }
 
-  protected getPublicKey(): string | undefined {
-    return undefined
-  }
+  async signChallenge(message: string): Promise<Signature> {
+    const hash = crypto.createHash('sha256')
+    hash.update(message, 'utf8')
+    const hashedMessage = hash.digest('base64')
 
-  signChallenge(message: string): string {
-    return ''
+    const signature = ec.keyFromPrivate(privateKey).sign(hashedMessage)
+    return {
+      r: signature.r.toString('hex'),
+      s: signature.s.toString('hex'),
+      recoveryParam: signature.recoveryParam as number,
+    }
   }
 }
