6dcc454353b62de0f687a0786adec1655022fe5f
osdnk <micosa97@gmail.com>
2023-03-09 15:38:09 +0200

    tests fixes

--- capsule/src/test/quasitests.ts
+++ capsule/src/test/quasitests.ts
@@ -2,25 +2,30 @@ import {
   completeFlowWithServer,
   completeFlowOffline,
 } from './ChallengeStorage.quasitest';
-import { completeFlowWithServerTwoKeys } from './ChallengeStorageDifferentKeys.quasitest';
-import { keyRecoveryFlow } from './KeyRecovery.quasitest';
-import { keyRefreshFlow } from './KeyRefresh.quasitest';
-import { loginFlow } from './LoginFlow.quasitest';
-import { privateKeyStoringFlow } from './PrivateKeyStorage.quasitest';
-import { recoverRecoveryShare } from './RecoverRecovery.quasitest';
-import { transmissionFlow } from './Transimission.quasitest';
+import {completeFlowWithServerTwoKeys} from './ChallengeStorageDifferentKeys.quasitest';
+import {keyRecoveryFlow} from './KeyRecovery.quasitest';
+import {keyRefreshFlow} from './KeyRefresh.quasitest';
+import {loginFlow} from './LoginFlow.quasitest';
+import {privateKeyStoringFlow} from './PrivateKeyStorage.quasitest';
+import {recoverRecoveryShare} from './RecoverRecovery.quasitest';
+import {transmissionFlow} from './Transimission.quasitest';
 
 async function runTests() {
-  await completeFlowWithServer();
-  await completeFlowOffline();
-  await loginFlow();
-  await privateKeyStoringFlow();
-  await completeFlowWithServerTwoKeys();
-  await recoverRecoveryShare();
-  await keyRecoveryFlow();
-  await keyRefreshFlow();
-  await transmissionFlow();
-  console.log('ALL TESTS PASSED');
+  try {
+    await completeFlowWithServer();
+    await completeFlowOffline();
+    await loginFlow();
+    await privateKeyStoringFlow();
+    await completeFlowWithServerTwoKeys();
+    await recoverRecoveryShare();
+    await keyRecoveryFlow();
+    await keyRefreshFlow();
+    await transmissionFlow();
+  } catch (e) {
+    console.log('ERROR', e);
+  }
+
+  console.log('ALL TESTS ENDED');
 }
 
 runTests();
